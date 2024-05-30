309bbf71d0b59636044e0e989f5fa95226970577
QuangNguyenLab <quangnguyen4202@gmail.com>
2024-05-30 13:40:57 +0700

    Login All - Quang

---
+++ DataAccess/Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        
+
+
+    }
+}
+
+
+    
+
