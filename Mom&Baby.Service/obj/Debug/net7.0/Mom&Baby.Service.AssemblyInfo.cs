b5bc990649c177a9b7c469878f927b8047cfba5a
NXQuangWIn <92784897+NXQuangWIn@users.noreply.github.com>
2024-05-30 14:10:13 +0700

    test

--- Mom&Baby.Service/obj/Debug/net7.0/Mom&Baby.Service.AssemblyInfo.cs
+++ Mom&Baby.Service/obj/Debug/net7.0/Mom&Baby.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mom&Baby.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbad65e0841658a2df6cd47394d0eec321e3dc8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mom&Baby.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mom&Baby.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
